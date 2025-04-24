709984c2f51fadcf9d5076e6b0453409bebfd5d1
scv1702 <scv1702@naver.com>
2025-04-24 20:21:52 +0900

    CRT-50 chore: 더미 데이터 삽입 스크립트 작성

---
+++ src/main/resources/db/init/insert_characters.sql
@@ -0,0 +1,5 @@
+insert into story (id, title, description, story_type, image_url)
+values (1, "신데렐라", "신데렐라 이야기", "WESTERN", null);
+
+insert into characters (character_id, name, description, image_url, story_id)
+values (1, "cinderella", "신데렐라", null, 1);
