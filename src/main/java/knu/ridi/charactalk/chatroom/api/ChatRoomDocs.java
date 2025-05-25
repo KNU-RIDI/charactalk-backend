d5c6b04b14b370c1d62dde3d43d9389c909aa783
wodydl0 <poty9798@naver.com>
2025-05-25 23:34:52 +0900

    CRT-51 feat: api docs 작성

--- src/main/java/knu/ridi/charactalk/chatroom/api/ChatRoomDocs.java
+++ src/main/java/knu/ridi/charactalk/chatroom/api/ChatRoomDocs.java
@@ -6,10 +6,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import knu.ridi.charactalk.auth.service.CharactalkUser;
+import knu.ridi.charactalk.chatroom.api.dto.ChatRoomCardResponse;
 import knu.ridi.charactalk.chatroom.api.dto.CreateChatRoomRequest;
 import knu.ridi.charactalk.chatroom.api.dto.CreateChatRoomResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.List;
 
 @Tag(name = "채팅방")
 public interface ChatRoomDocs {
@@ -29,5 +34,29 @@ public interface ChatRoomDocs {
     ResponseEntity<CreateChatRoomResponse> createChatRoom(
         OAuth2User user,
         CreateChatRoomRequest request
+    );
+
+
+    @Operation(
+            summary = "채팅방 목록 조회",
+            description = "로그인한 사용자가 참여 중인 채팅방 목록을 조회합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "채팅방 목록 조회 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ChatRoomCardResponse.class,
+                                            type = "array"
+                                    )
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "500", description = "서버 에러")
+            }
+    )
+    ResponseEntity<List<ChatRoomCardResponse>> getChatRooms(
+            @AuthenticationPrincipal CharactalkUser user
     );
 }
